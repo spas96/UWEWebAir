a7a8900747a12ce295250d9a7b474d1687e60f2b
Spas Spasov <s.spasov96@abv.bg>
2016-02-11 05:15:29 +0000

    Update javascript.js

--- template/javascript.js
+++ template/javascript.js
@@ -1,9 +1,32 @@
-$(document).ready(function () {
-	"use strict"
+jQuery(function($){ 
 
-
-  
-	$('.send-button').click(function () {
+var datefield=document.createElement("input")
+    datefield.setAttribute("type", "date")
+        document.write('<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />\n')
+        document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"><\/script>\n')
+        document.write('<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"><\/script>\n')
+		
+			$( ".start_date" ).datepicker({
+				onClose: function( selectedDate ) {
+				$( ".end_date" ).datepicker( "option", "minDate", selectedDate );
+			},
+			 beforeShowDay: function(date) {
+				var day = date.getDay();
+				return [day != 1];
+			},
+			minDate: 0
+			});
+			$( ".end_date" ).datepicker({
+				onClose: function( selectedDate ) {
+				$( ".start_date" ).datepicker( "option", "maxDate", selectedDate );
+			  },
+			 beforeShowDay: function(date) {
+				var day = date.getDay();
+				return [day != 1];
+			}
+			});
+			
+				$('.send-button').click(function () {
 		var userDateInput1 = $('.start_date');
 		var userDate1 = userDateInput1.val();
 		var userDateInput2 = $('.end_date');
@@ -21,5 +44,5 @@ $(document).ready(function () {
 		
 		}
 	});
-
-});
+	
+})
