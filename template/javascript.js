b09a789d7db17862834208d6c75a1fdc5a1c36bc
Spas Spasov <s.spasov96@abv.bg>
2016-02-12 04:37:46 +0000

    Update javascript.js

--- template/javascript.js
+++ template/javascript.js
@@ -1,29 +1,25 @@
-jQuery(function($){ 
-			
-		$('.send-button').click(function () {
+$(document).ready(function () {
+	"use strict"
+
+
+  
+	$('.send-button').click(function () {
 		var userDateInput1 = $('.start_date');
 		var userDate1 = userDateInput1.val();
 		var userDateInput2 = $('.end_date');
 		var userDate2 = userDateInput2.val();
-		
-		var userDestInput1 = $('.start_dest');
-		var userDest1 = userDestInput1.val();
-		var userDestInput2 = $('.end_dest');
-		var userDest2 = userDestInput2.val();
 		var search_box_form = $('.search-box');
 		search_box_form.submit(function(e) {
     			e.preventDefault();
 		});
 		
-		userDateInput1.keydown(function(e){
-			e.preventDefault();
-		);
+		
 	
-		if(userDate1!="" && userDate2!="" && userDest1!="" && userDest2!=""){
+		if(userDate1!="" && userDate2!=""){
 		
 							alert("Looking for tickets..");
 		
 		}
 	});
-	
-})
+
+});
