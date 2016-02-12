874613c9a48f0dd0a6bb73f9a740e83285eac70a
Spas Spasov <s.spasov96@abv.bg>
2016-02-12 04:36:52 +0000

    Update javascript.js

--- template/javascript.js
+++ template/javascript.js
@@ -1,44 +1,25 @@
 jQuery(function($){ 
-
-var datefield=document.createElement("input")
-    datefield.setAttribute("type", "date")
-        document.write('<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />\n')
-        document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"><\/script>\n')
-        document.write('<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"><\/script>\n')
-		
-			$( ".start_date" ).datepicker({
-				onClose: function( selectedDate ) {
-				$( ".end_date" ).datepicker( "option", "minDate", selectedDate );
-			},
-			 beforeShowDay: function(date) {
-				var day = date.getDay();
-				return [day != 1];
-			},
-			minDate: 0
-			});
-			$( ".end_date" ).datepicker({
-				onClose: function( selectedDate ) {
-				$( ".start_date" ).datepicker( "option", "maxDate", selectedDate );
-			  },
-			 beforeShowDay: function(date) {
-				var day = date.getDay();
-				return [day != 1];
-			}
-			});
 			
-				$('.send-button').click(function () {
+		$('.send-button').click(function () {
 		var userDateInput1 = $('.start_date');
 		var userDate1 = userDateInput1.val();
 		var userDateInput2 = $('.end_date');
 		var userDate2 = userDateInput2.val();
+		
+		var userDestInput1 = $('.start_dest');
+		var userDest1 = userDestInput1.val();
+		var userDestInput2 = $('.end_dest');
+		var userDest2 = userDestInput2.val();
 		var search_box_form = $('.search-box');
 		search_box_form.submit(function(e) {
     			e.preventDefault();
 		});
 		
-		
+		userDateInput1.keydown(function(e){
+			e.preventDefault();
+		);
 	
-		if(userDate1!="" && userDate2!=""){
+		if(userDate1!="" && userDate2!="" && userDest1!="" && userDest2!=""){
 		
 							alert("Looking for tickets..");
 		
