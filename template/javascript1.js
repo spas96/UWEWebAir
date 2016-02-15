9f3de0f98016012a376c8db80f94256eed3adfbc
Spas Spasov <s.spasov96@abv.bg>
2016-02-15 03:45:54 +0000

    Update javascript1.js

--- template/javascript1.js
+++ template/javascript1.js
@@ -17,15 +17,36 @@ $(function()
 	  var resultsDiv1 = document.getElementById("results1");
       var searchField1 = form.start_dest;
 	  var searchField2 = form.end_dest;
-	  
-
-
       
       // now, attach the keyup handler to the search field:
+		if(document.getElementById('radio1').checked){
+			var myElement = document.querySelector(".end_date");
+			var myElementReq = document.querySelector(".end_date_required");
+				myElement.style.backgroundColor = "#bbb";
+				myElement.setAttribute("disabled", "true"); 
+				myElementReq.removeAttribute("required");
+			
+		}
+		document.getElementById('radio1').onchange = function() {
+			var myElement = document.querySelector(".end_date");
+			var myElementReq = document.querySelector(".end_date_required");
+				myElement.style.backgroundColor = "#bbb";
+				myElement.setAttribute("disabled", "true"); 
+				myElementReq.removeAttribute("required");
+		}
 
+		document.getElementById('radio2').onchange = function() {
+			var myElement = document.querySelector(".end_date");
+			var myElementReq = document.querySelector(".end_date_required");
+			if((searchField1.value=="Bristol" && searchField2.value=="London") || (searchField1.value=="London" && searchField2.value=="Manchester") || (searchField1.value=="Bristol" && searchField2.value=="Glasgow") || (searchField1.value=="Glasgow" && searchField2.value=="Newcastle") || (searchField1.value=="Newcastle" && searchField2.value=="Manchester")){
+			}else{
+				myElement.style.backgroundColor = "white";
+				myElement.removeAttribute("disabled");
+				myElementReq.setAttribute("required", "true");
+			}
+		}
       searchField1.onkeyup = function()
       {
-
 
           var txt = this.value.toLowerCase();
           if ( txt.length == 0 ) return;
